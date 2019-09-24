38ad818c6433ef266b86be0d86bfa9123c8d4621
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-24 05:45:27 -0700

    Custom add middleware and jwt verification

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // if token does not exist
+  if (!token) {
+    return res.status(401).json({ msg: 'Token missing, authorization denied!' });
+  }
+
+  // if there is a token
+  try {
+    const decoded = jwt.verify(token, config.get('secretOrKey'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is invalid!' });
+  }
+}
