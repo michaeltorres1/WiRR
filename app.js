c3faecc881c978b7753205df648a68689e29334b
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-24 04:43:45 -0700

    Request and Body Validation

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,29 @@
 
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route     GET api/users
-// @desc      Test route
+// @route     POST api/users
+// @desc      Register user
 // @access    Public
-router.get('/', (req, res) => res.send('Testing User Route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'A valid email is required').isEmail(),
+    check('password', 'A password of 6 or more characters is required').isLength({ min: 6 })
+  ],
+  (req, res) => {
+    // Handle response
+    const errors = validationResult(req);
+
+    // check for errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('Users...');
+  }
+);
 
 module.exports = router;
