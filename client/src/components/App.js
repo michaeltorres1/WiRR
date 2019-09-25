872dfd14d985bd27ae0e5aa5b48f8418734dd681
Hakeem Almidan <hakeemmidan@gmail.com>
2019-09-24 17:06:18 -0700

    Include functioning helper methods and non-functioning React component

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WikiUrlInput } from './wiki_url_input/wiki_url_input';
+
+function App() {
+  return (
+    <div>
+      <WikiUrlInput />
+    </div>
+  );
+}
+
+export default App;
