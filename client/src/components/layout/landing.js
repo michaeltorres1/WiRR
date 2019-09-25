9c65f6954f33829e7b7c19d8e1596f8d44d396dd
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-25 16:32:22 -0700

    Add Landing page design

---
+++ client/src/components/layout/landing.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { WikiUrlInput } from '../wiki_url_input/wiki_url_input';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Wikipedia Reliability Rater</h1>
+          <p className="lead">
+            Enter a wikipedia link or keyword to search the Reliability
+            of the article
+          </p>
+          <div className="buttons">
+            <WikiUrlInput />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Landing
