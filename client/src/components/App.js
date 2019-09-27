c4a1a206e35c958489a73fd24b25a558a9e5c4c6
KaminKevCrew <durgeisawesome@gmail.com>
2019-09-27 15:57:16 -0700

    Add Show Page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './layout/navbar';
 import Landing from './layout/landing';
 import Register from './auth/Register';
+import Show from './charts/show_page';
 import Login from './auth/Login';
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -33,6 +34,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
+              <Route exact path="/articles" component={ Show } />
             </Switch>
           </section>
         </div>
