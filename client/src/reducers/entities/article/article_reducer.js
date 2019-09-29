e365e1caa904877a9d41c7f5e835e9a1b492a85d
Hakeem Almidan <hakeemmidan@gmail.com>
2019-09-29 13:54:14 -0700

    Create entities reducer and add article reducer to it

--- client/src/reducers/entities/article/article_reducer.js
+++ client/src/reducers/entities/article/article_reducer.js
@@ -1,13 +1,21 @@
-import {RECEIVE_ARTICLE, DELETE_ARTICLE} from '../../../actions/types';
+import {
+  RECEIVE_ARTICLE,
+  DELETE_ARTICLE,
+  RECEIVE_VISIT_PAGE
+} from '../../../actions/types';
 
 const articleReducer = (state = {}, action) => {
   Object.freeze(state);
+  let newState;
   switch (action.type) {
     case RECEIVE_ARTICLE:
       return Object.assign({}, state, action.article.data);
     case DELETE_ARTICLE:
-      let newState = Object.assign({}, state);
+      newState = Object.assign({}, state);
       delete newState[action.articleId];
+      return newState;
+    case RECEIVE_VISIT_PAGE:
+      newState = Object.assign({}, state, action.response);
       return newState;
     default:
       return state;
