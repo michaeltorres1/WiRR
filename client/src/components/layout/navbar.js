b293437287c61360c45dc2c4476d8f1e78b2ee94
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-29 00:51:45 -0700

    Add comment header to navbar.js

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -1,3 +1,11 @@
+/*
+Author: Michael Torres
+Filename: navbar.js
+Description: This file is the component for the navbar across the website
+             and contains the website name, sign up and sign in links and
+             a hamburger menu when size desktop size is under 700 pixels
+*/
+
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
