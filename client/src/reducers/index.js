ec0d80d3184e46854805755e3bca1ebfaf39af16
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-30 15:54:45 -0700

    Include ui reducer in root reducer (index.js)

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import auth from './auth';
 import alert from './alert_reducer';
 import { articleReducer } from './entities/article/article_reducer';
 import { entitiesReducer } from './entities/entities_reducer';
+import ui from './ui/ui_reducer';
 
 export default combineReducers ({
   entities: entitiesReducer,
   auth,
   alert,
-  article: articleReducer
+  article: articleReducer,
+  ui
 });
