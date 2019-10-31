2c4406a7c0f0993935da74cfb308dc0434850c09
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-30 19:08:01 -0700

    Add all contributors and personal links in each row

--- client/src/components/modal/team_personal_info.jsx
+++ client/src/components/modal/team_personal_info.jsx
@@ -15,6 +15,72 @@ export function TeamPersonalInfo() {
                     <div>
                         <div className="personal-account-links">
                             <a
+                                href="https://github.com/michaeltorres1"
+                                target="_blank">
+                                <GithubIcon />
+                            </a>
+                            &nbsp;
+                            <a
+                                href="https://www.linkedin.com/in/michael-torres-8649aa178/"
+                                target="_blank">
+                                <LinkedInIcon />
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li
+                    className="team-personal-info-row"
+                    key={'team-personal-info-row-1'}>
+                    <div>
+                        Lwin Ye
+                    </div>
+                    <div>
+                        <div className="personal-account-links">
+                            <a
+                                href="https://github.com/tokyoanime"
+                                target="_blank">
+                                <GithubIcon />
+                            </a>
+                            &nbsp;
+                            <a
+                                href="https://www.linkedin.com/in/lwin-ye-797a2558/"
+                                target="_blank">
+                                <LinkedInIcon />
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li
+                    className="team-personal-info-row"
+                    key={'team-personal-info-row-1'}>
+                    <div>
+                        Kevin Kaminski
+                    </div>
+                    <div>
+                        <div className="personal-account-links">
+                            <a
+                                href="https://github.com/KaminKevCrew"
+                                target="_blank">
+                                <GithubIcon />
+                            </a>
+                            &nbsp;
+                            <a
+                                href="https://www.linkedin.com/in/kevin-kaminski-850991b7/"
+                                target="_blank">
+                                <LinkedInIcon />
+                            </a>
+                        </div>
+                    </div>
+                </li>
+                <li
+                    className="team-personal-info-row"
+                    key={'team-personal-info-row-1'}>
+                    <div>
+                        Hakeem Almidan
+                    </div>
+                    <div>
+                        <div className="personal-account-links">
+                            <a
                                 href="https://github.com/Hakeemmidan"
                                 target="_blank">
                                 <GithubIcon />
@@ -27,9 +93,6 @@ export function TeamPersonalInfo() {
                             </a>
                         </div>
                     </div>
-                </li>
-                <li>
-
                 </li>
             </ul>
         </div>
